0f3f2ad653109e6816066222372429404deac339
FelipeLp <felopesserra@id.uff.br>
2024-10-24 16:01:13 -0300

    Cria migration que adiciona IsUnique aos campos (Turma) Codigo e (Aluno) Email

---
+++ Migrations/20241024185803_ApplyFieldIsUnique.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApiC_.Migrations
+{
+    /// <inheritdoc />
+    public partial class ApplyFieldIsUnique : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Turmas_Codigo",
+                table: "Turmas",
+                column: "Codigo",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Alunos_Email",
+                table: "Alunos",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Turmas_Codigo",
+                table: "Turmas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Alunos_Email",
+                table: "Alunos");
+        }
+    }
+}
